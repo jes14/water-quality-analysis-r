a92a7384aed84d3ff42a08b28a99984bd0214c24
jes14 <yj25058@gmail.com>
2024-07-15 09:19:56 +0200

    add plots

--- script/analysis.R
+++ script/analysis.R
@@ -11,6 +11,47 @@ plot <- ggplot(data, aes(x = Timestamp, y = `Average Water Speed`)) +
   labs(title = "Average Water Speed Over Time", x = "Timestamp", y = "Average Water Speed") +
   theme_minimal()
 
-ggsave("./plots/plot.png", plot = plot)
+# ggsave("./plots/plot.png", plot = plot)
 
 print(plot)
+
+# Creating a ggplot
+ggplot <- ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+# ggsave("./plots/ggplot.png", plot = ggplot)
+
+print(ggplot)
+
+
+lineggplot <- ggplot(data = mpg) + 
+  geom_smooth(mapping = aes(x = displ, y = hwy, linetype = drv))
+
+# ggsave("./plots/lineggplot.png", plot = lineggplot)
+print(lineggplot)
+
+
+## Filter the data by month (August 2023)
+
+data$Timestamp <- as.POSIXct(data$Timestamp, format="%Y-%m-%d %H:%M:%S")
+
+colnames(data) <- make.names(colnames(data))
+
+data$Timestamp <- as.POSIXct(data$Timestamp, format="%Y-%m-%d %H:%M:%S")
+
+august_data <- data %>%
+  filter(format(Timestamp, "%Y-%m") == "2023-08")
+
+print(colnames(data))
+print(summary(august_data))
+
+augustggplot <- ggplot(august_data, aes(x=Timestamp, y=Average.Water.Speed)) +
+  geom_line() +
+  labs(title="Average Water Speed in August 2023",
+       x="Timestamp",
+       y="Average Water Speed") +
+  theme_minimal()
+
+print(augustggplot)
+
+# ggsave("./plots/augustggplot.png", plot = augustggplot)
